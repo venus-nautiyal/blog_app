3937f6f901a9e6cfb09810711539cd7f3df73300
Venus <venus.nautiyal@gmail.com>
2017-02-23 21:57:22 +1100

    Initiate blog_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '982d8316f132e6926a1ace82b7ca7e385043497e775b6b15cff2ac297c58ae1e0aaae72bf2f91dfc8728b9dae58d523ddbea23ff1de3f2c4b0386ba458655ff5'
